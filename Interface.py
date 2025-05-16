69a89f911fb5463651c1a83ed4461c6242dc03ff
Aurélien LITRA <aurelien.litra@gmail.com>
2025-05-16 14:31:41 +0200

    Ajout des differents bouttons

--- Interface.py
+++ Interface.py
@@ -7,9 +7,27 @@ class ControlPanel(QWidget):
         super().__init__()
         self.setWindowTitle("Interface de pilotage du robot Marty")
         self.setFixedSize(800, 800)
+        self.interface()
+
+    def interface(self):
+        layout = QVBoxLayout()
+
+        self.bouton_avancer = QPushButton("Avancer")
+        self.bouton_reculer = QPushButton("Reculer")
+        self.bouton_droite = QPushButton("Aller à droite")
+        self.bouton_gauche = QPushButton("Aller à gauche")
+        
+        layout.addWidget(self.bouton_avancer)
+        layout.addWidget(self.bouton_reculer)
+        layout.addWidget(self.bouton_droite)
+        layout.addWidget(self.bouton_gauche)
+
+        self.setLayout(layout)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     fenetre = ControlPanel()
     fenetre.show()
     app.exec()
+
