b174faef862de3b6a4ac8c1caa9443aa303be258
Mpucheu2 <mathiaspucheu2@gmail.com>
2025-05-16 17:16:30 +0200

    Creation of eyes expression
    Creation of all eyes expression movement function.

--- Expression.py
+++ Expression.py
@@ -1,42 +1,45 @@
 from martypy import Marty
 import time
-
 adresse_ip = "192.168.0.101" 
 marty = Marty("wifi", adresse_ip) 
-
-def move_forward():
+def blink():
     if(marty.is_conn_ready):
-        marty.walk(2,'auto',0,-25,1500)
+        marty.eyes("angry",500)
+        time.sleep(0.5)
+        marty.eyes("normal",500)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-def move_left():
+def angry():
     if(marty.is_conn_ready):
-        marty.sidestep("left",1,35,1000,)
-    else:
-        print("Marty is sadely not connected")
-        return 0
-    
-def move_right():
-    if(marty.is_conn_ready):
-        marty.sidestep("right",1,35,1000,)
-    else:
-        print("Marty is sadely not connected")
-        return 0
-
-def move_backward():
-    if(marty.is_conn_ready):
-        marty.walk(2,'auto',0,25,1500)
+        marty.eyes("angry",500)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-for i in range(10):
-    move_forward()
-    move_backward()
-for i in range(10):
-    move_left() 
-    move_right()
+def wide_open():
+    if(marty.is_conn_ready):
+        marty.eyes("wide",500)
+    else:
+        print("Marty is sadely not connected")
+        return 0
+    return 0
+
+def excited():
+    if(marty.is_conn_ready):
+        marty.eyes("excited",500)
+    else:
+        print("Marty is sadely not connected")
+        return 0
+    return 0
+
+def wiggle():
+    if(marty.is_conn_ready):
+        marty.eyes("wiggle",1000)
+    else:
+        print("Marty is sadely not connected")
+        return 0
+    return 0
