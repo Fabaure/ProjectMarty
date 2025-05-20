3c6ec6f3d0c460f9f1b420711f08fcd3b3a442c9
Aurélien LITRA <aurelien.litra@gmail.com>
2025-05-20 11:53:13 +0200

    Upload main

--- main.py
+++ main.py
@@ -2,14 +2,7 @@ from Interface import *
 from martypy import Marty, MartyConnectException
 
 if __name__ == "__main__":
-    adresse_ip = "192.168.0.100" 
-    try:
-        marty = Marty("wifi", adresse_ip) 
-        print("Marty connecté !")
-    except MartyConnectException as e:
-        print("Impossible de se connecter à Marty")
-        marty = None
     app = QApplication(sys.argv)
-    fenetre = ControlPanel(marty)
+    fenetre = ControlPanel(None)
     fenetre.show()
     app.exec()
