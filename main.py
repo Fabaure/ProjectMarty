f4cf31a98827798249fa5a79b5b07a594a023788
Craftsman0001 <131674449+Craftsman0001@users.noreply.github.com>
2025-05-20 09:35:56 +0200

    recalibrage des couleurs, ajout de la touche A pour arrêter le program et E pour récupérer les données des capteurs

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+import msvcrt
 from martypy import Marty
 from sensor import *
 
 def main():
-    adresse_ip = "192.168.0.101" # à modifier
+    adresse_ip = "192.168.0.108" # à modifier
     marty = Marty("wifi", adresse_ip) # connexion à Marty
     marty.set_marty_name("KAY/0")
 
     if (marty.is_conn_ready()): 
         # si Marty est connecté
         print("Connected to Marty !")
-        while(marty.is_conn_ready()): # boucle si Marty connecté
-            getColor(marty)
-            getDistance(marty)
-            getBattery(marty)
-            # code a faire
-            marty.close() # deconnection de Marty
-            print("Disconnected from Marty.")
+        running = True
+        while(marty.is_conn_ready() and running): # boucle si Marty connecté
+            if (msvcrt.kbhit()):  # une touche a été pressée
+                key = msvcrt.getch()
+                if key == b'a' or key == b'A':
+                    print("Touche A détectée, arrêt.")
+                    running = False
+                if key == b'e' or key == b'E':
+                    getColor(marty)
+                    getDistance(marty)
+                    getBattery(marty)
+        print("Disconnected from Marty.")
+        marty.close() # deconnection de Marty
     else: 
         # si Marty n'est pas connecté
         print("Failed to connect to Marty T-T.")
--- sensor.py
+++ sensor.py
@@ -1,22 +1,24 @@
 def getColor(marty):
+    # recupere la couleure sous marty
     color = marty.get_ground_sensor_reading("left") # lecture de la couleur
     print("Color : " + str(color))
-    if(color < 20):
+    if(color < 19):
         print("Black")
-    elif(color > 20 and color < 25):
+    elif(color > 19 and color < 27):
         print("Blue")
-    elif(color > 30 and color < 35):
+    elif(color > 27 and color < 37):
         print("Green")
-    elif(color > 45 and color < 55):
+    elif(color > 45 and color < 57):
         print("Cyan")
     elif(color > 75 and color < 90):
         print("Red")
-    elif(color > 90 and color < 105):
+    elif(color > 90 and color < 110):
         print("Magenta")
-    elif(color > 180 and color < 195):
+    elif(color > 180 and color < 200):
         print("Yellow")
 
 def getDistance(marty):
+    # recupere la distance devant marty
     distance = marty.get_obstacle_sensor_reading("right") # recupere la distance en millimetres
     print("Distance : " + str(distance))
     if(distance != 0):
@@ -25,6 +27,7 @@ def getDistance(marty):
         print("Obstacle : No")
 
 def getBattery(marty):
+    # recupere le pourcentage de la batterie de marty
     battery = marty.get_battery_remaining()
     print("Battery : " + str(battery) + "%")
     
