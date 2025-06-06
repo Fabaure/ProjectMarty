2d9820671bfb22293f17ae4aa1ad47d2d782ea13
Aurélien LITRA <aurelien.litra@gmail.com>
2025-06-06 11:52:41 +0200

    Ajout des fonct de mathias

--- Expression.py
+++ Expression.py
@@ -1,51 +1,47 @@
-from martypy import Marty
 import time
-addresse_ip = "192.168.0.101" 
-marty = Marty("wifi", addresse_ip) 
 
-
-def angry():
-    if(marty.is_conn_ready):
+def angry(marty):
+    if(marty):
         marty.eyes("angry",500) # Eyes take ether a name of preprogramed expression and a time of execution or an angle of rotation and a time of execution
     else:
         print("Marty is sadely not connected") # Print an error when not connected
         return 0
     return 0
 
-def wide_open():
-    if(marty.is_conn_ready):
+def wide_open(marty):
+    if(marty):
         marty.eyes("wide",500)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-def excited():
-    if(marty.is_conn_ready):
+def excited(marty):
+    if(marty):
         marty.eyes("excited",500)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-def wiggle():
-    if(marty.is_conn_ready):
+def wiggle(marty):
+    if(marty):
         marty.eyes("wiggle",1000)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-def eyes_control(angle,time_in_ms):
-    if(marty.is_conn_ready):
+def eyes_control(marty,angle,time_in_ms):
+    if(marty):
         marty.eyes(angle,time_in_ms)
     else:
         print("Marty is sadely not connected")
         return 0
     return 0
 
-def color_control(color,pattern,time_set):
-    if(marty.is_conn_ready):
+def color_control(marty,color,pattern,time_set):
+    if(marty):
         marty.disco_pattern(int(pattern))
         marty.disco_color(color)
         time.sleep(int(time_set))
@@ -54,8 +50,8 @@ def color_control(color,pattern,time_set):
         return 0
     return 0
 
-def feel_control(color,emotion):
-    if(marty.is_conn_ready):
+def feel_control(marty,color,emotion):
+    if(marty):
         marty.disco_color(color)
         marty.eyes(emotion,500)
         time.sleep(2)
@@ -64,7 +60,3 @@ def feel_control(color,emotion):
         print("Marty is sadely not connected")
         return 0
     return 0
-
-feel_control("4080ff","wiggle")
-
-marty.close()
