842bfa820859118bc94f0de09d08675ee12a6439
Aurélien LITRA <aurelien.litra@gmail.com>
2025-06-06 10:00:45 +0200

    Fix bug

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from Interface import *
+from mainWindow import *
 from martypy import Marty, MartyConnectException
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    fenetre = ControlPanel(None)
+    fenetre = MainWindow(None)
     fenetre.show()
     app.exec()
