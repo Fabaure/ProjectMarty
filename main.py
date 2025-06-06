b787fe9e903060a0f20a946e368323a5c1f610a4
YS <testdevys@gmail.com>
2025-06-06 08:58:51 +0200

    Ajout des informations du robot dans mainWindow
    Ajout du contrôle de l'adresse IP dans mainWindow, ainsi que des informations relatives au robot

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ from creationPanel import CreationPanel
 from mainWindow import MainWindow
 
 if __name__ == "__main__":
-    adresse_ip = "192.168.0.100" 
-    try:
-        marty = Marty("wifi", adresse_ip) 
-        print("Marty connecté !")
-    except MartyConnectException as e:
-        print("Impossible de se connecter à Marty")
-        marty = None
     app = QApplication(sys.argv)
-    fenetre = MainWindow(marty)
+    fenetre = MainWindow(None)
     fenetre.show()
     app.exec()
