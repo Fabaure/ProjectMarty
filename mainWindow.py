39b9060f6d5f3e944e086aa1899452ca927d9b82
YS <testdevys@gmail.com>
2025-06-10 13:55:27 +0200

    Ajout d'un panneau de contrôle pour jouer des sons avec Marty
    Ajout d'un fichier playSound qui contient les fonctions pour envoyer et lire des sons à Marty.
    Ajout de dans mainWindow de sound panel dans le menu permettant de controler Marty.
    Création de Sound Panel qui nous permet de lire un son mp3 stocké dans le projet ou de jouer des preset prédéfinis

--- mainWindow.py
+++ mainWindow.py
@@ -3,6 +3,7 @@ from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPu
 from martypy import Marty
 from controlPanel import ControlPanel
 from creationPanel import CreationPanel
+from soundPanel import SoundPanel
 from sensor import *
 
 class MainWindow(QWidget):
@@ -13,7 +14,9 @@ class MainWindow(QWidget):
 
         self.setStyleSheet("""
             QPushButton { padding: 5px;  border-radius:10px; background-color: #fff; }
+            QPushButton:pressed { background-color: #404040; color: #fff }
             QGroupBox { font-size: 18px }
+            
         """)
 
 
@@ -21,25 +24,31 @@ class MainWindow(QWidget):
 
         self.control_panel = ControlPanel(self.marty)
         self.creation_panel = CreationPanel(self.marty)
+        self.sound_panel = SoundPanel(self.marty)
 
         self.stack = QStackedWidget()
         self.stack.addWidget(self.control_panel)
         self.stack.addWidget(self.creation_panel)
+        self.stack.addWidget(self.sound_panel)
 
         self.btn_control = QPushButton("Control Panel")
         self.btn_creation = QPushButton("Creation Panel")
+        self.btn_sound = QPushButton("Sound Panel")
+
+        
+        # Connexion layout 
 
         connexion_layout = QGridLayout()
-        self.label = QLabel("Adresse IP :")
-        self.input_field = QLineEdit()
 
         # Choose IP Address
+        self.label = QLabel("Adresse IP :")
+        self.ip_address_input_field = QLineEdit()
+        self.connexionButton = QPushButton("Ok")
 
-        self.button = QPushButton("Ok")
-        self.button.clicked.connect(self.connect)
+        self.connexionButton.clicked.connect(self.connect) # When connexion button is clicked, it calls the function connect
         connexion_layout.addWidget(self.label)
-        connexion_layout.addWidget(self.input_field)
-        connexion_layout.addWidget(self.button)
+        connexion_layout.addWidget(self.ip_address_input_field)
+        connexion_layout.addWidget(self.connexionButton)
         connexion = QGroupBox("Connexion")
         connexion.setLayout(connexion_layout)
         
@@ -54,14 +63,18 @@ class MainWindow(QWidget):
         robot_info.addWidget(self.battery_label)
         robot_info.addWidget(self.statut_robot)
         
-
+        # choosing menu panel, button connectivity
         self.btn_control.clicked.connect(lambda: self.stack.setCurrentIndex(0))
         self.btn_creation.clicked.connect(lambda: self.stack.setCurrentIndex(1))
+        self.btn_sound.clicked.connect(lambda: self.stack.setCurrentIndex(2))
 
+        # choosing panel set up 
         btn_layout = QHBoxLayout()
         btn_layout.addWidget(self.btn_control)
         btn_layout.addWidget(self.btn_creation)
-
+        btn_layout.addWidget(self.btn_sound)
+        
+        # main page screen
         main_layout = QVBoxLayout()
         main_layout.addLayout(btn_layout)
         main_layout.addWidget(connexion)
@@ -73,7 +86,7 @@ class MainWindow(QWidget):
 
 
     def connect(self):
-        texte = self.input_field.text()
+        texte = self.ip_address_input_field.text()
         try:
             marty = Marty("wifi", texte) 
             print("Marty connecté !")
@@ -95,14 +108,17 @@ class MainWindow(QWidget):
         # Remove old panels
         self.stack.removeWidget(self.control_panel)
         self.stack.removeWidget(self.creation_panel)
+        self.stack.removeWidget(self.sound_panel)
 
         # Create new panels with the updated marty
         self.control_panel = ControlPanel(marty)
         self.creation_panel = CreationPanel(marty)
+        self.sound_panel = SoundPanel(marty)
 
         # Add new panels
         self.stack.addWidget(self.control_panel)
         self.stack.addWidget(self.creation_panel)
+        self.stack.addWidget(self.sound_panel)
 
 
 
