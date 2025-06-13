4cdff546a064bfe04366f992bbc33e2c233413df
YS <testdevys@gmail.com>
2025-06-13 16:59:24 +0200

    Gestion d'erreur dans set_volume_from_text

--- soundPanel.py
+++ soundPanel.py
@@ -99,8 +99,11 @@ class SoundPanel(QWidget):
 
     def set_volume_from_text(self) : 
         choosen_volume_to_change = self.choosing_volume_input_field.text() # Takes the input field value as a string because we need to apply the int() function
-        int_choosen_volume_to_change = int(choosen_volume_to_change) # Marty setVolume property takes int values
+        if not choosen_volume_to_change is None:
+            int_choosen_volume_to_change = int(choosen_volume_to_change) # Marty setVolume property takes int values
+            setVolumeFromInput(self.marty, int_choosen_volume_to_change); # Call the setVolumeFromInput function from playSound.py
+        else : 
+            print("Choosen Volume is None")
 
-        setVolumeFromInput(self.marty, int_choosen_volume_to_change); # Call the setVolumeFromInput function from playSound.py
 
 
