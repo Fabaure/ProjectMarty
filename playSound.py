e8170152ffef679d0b546b0178545fe495c7a166
YS <testdevys@gmail.com>
2025-06-13 16:54:16 +0200

    Ajout de commentaires des playSound.py et soundPanel.py, fonctionnalité pour choisir le volume voulu dans soundpanel
    Ajout de commmentaires dans playSound.py et dans soundPanel.py
    
    Ajout de de la possibilités permettant de choisir le volume de marty en choissisant la valeur voulue.

--- playSound.py
+++ playSound.py
@@ -1,67 +1,67 @@
 from martypy import Marty
 
 def progress_callback(bytes_sent: int, total_bytes: int) -> bool:
-    percent = (bytes_sent / total_bytes) * 100
-    print(f"Progression: {percent:.2f}% ({bytes_sent}/{total_bytes} octets)")
+    percent = (bytes_sent / total_bytes) * 100                                #Calcul how much percent of the file is already sent
+    print(f"Progression: {percent:.2f}% ({bytes_sent}/{total_bytes} octets)") #Print the value of last line
     return True 
 
 def rick_roll(marty):
-    marty.set_blocking(False)
-    print(marty.is_blocking())
+    marty.set_blocking(False) # Put blocking to false so marty can execute multiple things at the same time
+    print(marty.is_blocking()) # Verification of the value "is_blocking()"
     if(marty.is_conn_ready):
-        marty.set_volume(15)
         try:
-            success = marty.send_file("rick(1).mp3", progress_callback, file_dest="fs")
+            success = marty.send_file("rick(1).mp3", progress_callback, file_dest="fs") #Sent the file and print the progress
             if success:
                 print("Fichier envoyé avec succès ")
             else:
                 print("Envoi du fichier interrompu ")
         except Exception as e:
             print(f"Erreur pendant l'envoi : {e}")
-        marty.play_mp3("rick(1).mp3")
-        for i in range (10):
-            marty.sidestep("left",1,35,500,)
+        for i in range (10):            # Do the "Never gonna give you up" dance
+            marty.sidestep("left",1,35,1000,)
             marty.arms(45,-45,500)
-            marty.sidestep("right",1,35,500,)
+            marty.sidestep("right",1,35,1000,)
             marty.arms(-45,45,500)
+        marty.set_volume(35)            # Set the volume to a define percent
+        marty.play_mp3("rick(1).mp3")   # Play the file sent earlier (Actually not playing while dancing but after)
     else:
         print("Marty is sadely not connected")
         return 0
-    marty.set_blocking(True)
+    marty.set_blocking(True)        #Set blocking to true so it doesn't affect other function
     return 0
-
-
 
 def playChoosenSound(marty, choosenMusic):
     if(marty):
-        marty.set_blocking(False)
-        print(marty.is_blocking())
+        marty.set_blocking(False)   # Put blocking to false so marty can execute multiple things at the same time
+        print(marty.is_blocking())  # Verification of the value "is_blocking()"
         if(marty.is_conn_ready):
-            marty.set_volume(15)
             try:
-                success = marty.send_file(choosenMusic, progress_callback, file_dest="fs")
+                success = marty.send_file(choosenMusic, progress_callback, file_dest="fs")  #Sent the ChoosenMusic file and print the progress
                 if success:
                     print("Fichier envoyé avec succès ")
                 else:
                     print("Envoi du fichier interrompu ")
             except Exception as e:
                 print(f"Erreur pendant l'envoi : {e}")
-            marty.play_mp3(choosenMusic)
-            for i in range (10):
-                marty.sidestep("left",1,35,500,)
+            for i in range (10):             # Do the "Never gonna give you up" dance
+                marty.sidestep("left",1,35,1000,)
                 marty.arms(45,-45,500)
-                marty.sidestep("right",1,35,500,)
+                marty.sidestep("right",1,35,1000,)
                 marty.arms(-45,45,500)
+            marty.set_volume(35)            # Set the volume to a define percent
+            marty.play_mp3(choosenMusic)    # Play the file sent earlier (Actually not playing while dancing but after)
         else:
             print("Marty is sadely not connected")
             return 0
-        marty.set_blocking(True)
+        marty.set_blocking(True)    #Set blocking to true so it doesn't affect other function
     return 0
 
 def setVolumeFromInput(marty, choosenVolume): 
     if marty :
         if(marty.is_conn_ready):
-            marty.set_volume(choosenVolume)
+            marty.set_volume(choosenVolume) #S et the volume to a define percent
+
+            print(marty.get_volume())
             print("Nouveau volume")
         else :
             print("Marty is sadely not connected")
--- soundPanel.py
+++ soundPanel.py
@@ -18,10 +18,34 @@ class SoundPanel(QWidget):
             "Mii": "wiichan.mp3",
         }
 
-        # GroupBox contenant les boutons de sons
+        # |-----------------Part corresponding to the Input field corresponding to the choosing volume part ----------------------------------------------|
+        # GroupBox with sound button
         sound_box = QGroupBox("Choose a sound to play")
         sound_layout = QVBoxLayout()
 
+        choosingVolume_layout = QGridLayout()
+
+        # Set volume
+        self.choosing_volume_label = QLabel("Volume :")
+        self.choosing_volume_input_field = QLineEdit() # Create a input line to put text
+        self.choosing_volume_button = QPushButton("Choisir le volume")
+
+        # Create a layout where the widget will be put 
+        choosingVolume_layout.addWidget(self.choosing_volume_label)
+        choosingVolume_layout.addWidget(self.choosing_volume_input_field)
+        choosingVolume_layout.addWidget(self.choosing_volume_button)
+        # Create a box to contain these in the UI
+        choosing_volume = QGroupBox("Volume Set up")
+        choosing_volume.setLayout(choosingVolume_layout)
+
+        # Make the button with typing set the corresponding volume
+        self.choosing_volume_button.clicked.connect(self.set_volume_from_text )
+
+        layout.addWidget(choosing_volume)
+
+        # |-----------------------------------------------------------------------------------------------------------------------------------------|
+
+        #Put the appropriate sound for the right button
         for label, sound_id in self.preset_sounds_mp3.items():
             btn = QPushButton(label)
             btn.clicked.connect(lambda _, s=sound_id: self.play_sound(s))
@@ -30,9 +54,11 @@ class SoundPanel(QWidget):
         sound_box.setLayout(sound_layout)
         layout.addWidget(sound_box)
 
+
+        # |-----------------Part corresponding to the Input field corresponding to the choosen sound playing part ----------------------------------------------|
         choosingSound_layout = QGridLayout()
 
-        # Choose IP Address
+        # Choose Sound to play
         self.choosing_sound_label = QLabel("Quel fichier audio lire :")
         self.choosing_sound_input_field = QLineEdit() # Create a input line to put text
         self.choosing_sound_button = QPushButton("Jouer le son")
@@ -45,27 +71,36 @@ class SoundPanel(QWidget):
         choosing_sound = QGroupBox("Son personnalise")
         choosing_sound.setLayout(choosingSound_layout)
 
+        # Make the button with typing launch the corresponding sound
         self.choosing_sound_button.clicked.connect( self.play_sound_from_text )
 
         layout.addWidget(choosing_sound)
+        # |-----------------------------------------------------------------------------------------------------------------------------------------------------|
+
+        
         self.setLayout(layout)
 
-    def play_sound(self, sound_id):
+    def play_sound(self, sound_name):
         try:
             if self.marty:
-                playChoosenSound(self.marty, sound_id)
-                print(f"Playing sound: {sound_id}")
+                playChoosenSound(self.marty, sound_name) # Call the playChoosenSound function from playSound.py that play sound from the corresponding sound when the appropriate button is clicked
+                print(f"Playing sound: {sound_name}")
             else:
                 print("Marty not connected.")
         except Exception as e:
-            print(sound_id)
-            print(f"Error playing sound '{sound_id}': {e}")
+            print(f"Error playing sound '{sound_name}': {e}")
 
     def play_sound_from_text(self) :
-        sound_to_play = self.choosing_sound_input_field.text()
+        sound_to_play = self.choosing_sound_input_field.text() # Takes the input field value as a string
         try:
-            playChoosenSound(self.marty, sound_to_play)
+            playChoosenSound(self.marty, sound_to_play) # Call the playChoosenSound function from playSound.py that play sound from a string 
         except Exception as e:
             print(f"Error playing sound from text': {e}")
+
+    def set_volume_from_text(self) : 
+        choosen_volume_to_change = self.choosing_volume_input_field.text() # Takes the input field value as a string because we need to apply the int() function
+        int_choosen_volume_to_change = int(choosen_volume_to_change) # Marty setVolume property takes int values
+
+        setVolumeFromInput(self.marty, int_choosen_volume_to_change); # Call the setVolumeFromInput function from playSound.py
 
 
